f4d3ab3a0e3e770c21b1cb2a4be31f31378fb156
romcesar <romulo.coa@hotmail.com>
2024-08-15 18:06:05 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const { createServer } = require('node:http')
 
-const Produto = require('./routes/produtos');
-const rotas = require('./routes/router');
+const rotas = require('./src/routes/router');
 
-// Inicializando produtos
+const hostname = 'localhost';
+const port = 3000;
 
 
 // Implementando métodos para adicionar, remover e listar produtos.
-const server = http.createServer((req, res) => {
+const app = createServer((req, res) => {
 
   const { url, method } = req;
 
@@ -16,8 +16,8 @@ const server = http.createServer((req, res) => {
   if (url === '/') {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(JSON.stringify({ status: 'OK' }));
-
   }
+
 
   if (!rotas[url] || !rotas[url][method]) {
     res.writeHead(404, { "Content-Type": "application/jsonn" })
@@ -26,36 +26,8 @@ const server = http.createServer((req, res) => {
 });
 
 // Iniciando o servidor na porta 3000.
-const PORT = 3000;
-const HOST = '127.0.0.1';
 
-server.listen(PORT, HOST, () => {
-console.log(`Servidor rodando em http://${HOST}:${PORT}/`); });
+app.listen(port, hostname, () => {
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
 
-const http = require('http'); 
-const server = http.createServer((req, res) => { 
-
-  const { url, method } = req; 
-  
-  if (url === '/' && method === 'GET') { 
-    res.statusCode = 200; 
-    res.setHeader('Content-Type', 'text/plain'); 
-    res.end('Servidor Node.js'); 
-  }
-  else if (url === '/produtos' && method === 'GET') { 
-    res.statusCode = 200; 
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Listagem de Produtos'); 
-  } 
-  else { 
-    res.statusCode = 404; 
-    res.setHeader('Content-Type', 'text/plain'); 
-    res.end('Páginanão encontrada'); 
-  } 
-
-}); 
-
-const hostname = '127.0.0.1'; 
-const port = 3000; 
-server.listen(port, hostname, () => {
-console.log(`Servidor rodando em http://${hostname}:${port}/`); });
